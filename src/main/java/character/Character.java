a1b26a62cd6527603ff327d02a10966757066bb2
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-11-19 22:36:20 -0500

    Implemented Player and Enemy
    Added interface Inv, which ensures Inventory has all the methods Player and Enemy depend on
    
    Merged Equipment and Purchasable into one interface
    
    Moved test folder

--- src/main/java/character/Character.java
+++ src/main/java/character/Character.java
@@ -6,91 +6,30 @@ import inventory.*;
 public abstract class Character {
 
     /**
-     * An abstract class with some non abstract methods to be extended by Player and Enemy.
+     * An abstract class to be extended by Player and Enemy.
      */
 
     /**
-     * Instance Variables
+     * === Methods relating to health ===
      */
-    private final Inventory inv;
-
-    private int HPmax;
-
-    private int HP;
-
-    private int x;
-
-    private int y;
-
+    public abstract int getHP();
+    public abstract void setHP(int HP);
+    public abstract boolean isAlive();
 
     /**
-     * === Constructors ===
+     * === Methods relating to inventory ===
      */
-    public Character(Inventory inv, int HPmax, int x, int y) {
-        this.inv = inv;
-        this.HPmax = HPmax + inv.getArmor().getAttribute();
-        this.HP = HPmax;
-        this.x = x;
-        this.y = y;
-    }
-
+    public abstract Inventory getInventory();
 
     /**
-     * === Getters and Setters ===
+     * === Methods relating to position ===
      */
-    public Inventory getInventory(){
-        return inv;
-    }
-
-    public int getCoins() {
-        return this.inv.getCoins();
-    }
-
-    public int getHPmax() {
-        return this.HPmax;
-    }
-
-    public void changeHPmax(int upgrade) {
-        this.HPmax += upgrade;
-    }
-
-    public int getHP(){
-        return this.HP;
-    }
-
-    public void changeHP(int HP){
-        this.HP += HP;
-    }
-
-    public void setHP() {
-        this.HP = this.HPmax;
-    }
-
-    public int getx(){
-        return this.x;
-    }
-
-    public int gety(){
-        return this.y;
-    }
-
-    public void changex(int x) {
-        this.x += x;
-    }
-
-    public void changey(int y) {
-        this.y += y;
-    }
-
+    public abstract int getx();
+    public abstract int gety();
+    public abstract void setPosition(int x, int y);
 
     /**
-     * === Methods ===
+     * === Methods relating to other ===
      */
-    public int attack() {
-        return getInventory().getWeapon().getAttribute();
-    }
-
-    public boolean isAlive(){
-        return this.HP > 0;
-    }
+    public abstract int attack();
 }
--- src/main/java/character/Enemy.java
+++ src/main/java/character/Enemy.java
@@ -8,23 +8,67 @@ public class Enemy extends Character{
     /**
      * Instance Variables
      */
+    private final Inventory enemyInv;
+    private int HP;
 
-    /**
-     * === Constructor ===
-     */
+    private int x;
 
-    public Enemy(Inventory inv, int HPmax, int x, int y) {
-        super(inv, HPmax, x, y);
-    }
+    private int y;
+
 
     /**
      * === Getters and Setters ===
      */
 
+    public Inventory getInventory(){
+        return enemyInv;
+    }
+
+    public int getCoins() {
+        return this.enemyInv.getCoins();
+    }
+
+    public int getHP(){
+        return this.HP;
+    }
+
+    public void setHP(int HP){
+        this.HP += HP;
+    }
+
+    public int getx(){
+        return this.x;
+    }
+
+    public int gety(){
+        return this.y;
+    }
+
+    public void setPosition(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+
+    /**
+     * === Constructor ===
+     */
+
+    public Enemy(Inventory inventory, int HP){
+        this.enemyInv = inventory;
+        this.HP = HP + this.enemyInv.getArmor().getAttribute();
+    }
+
     /**
      * === Methods ===
      */
+    public int attack() {
+        return getInventory().getWeapon().getAttribute();
+    }
 
+    public boolean isAlive(){
+        return this.HP > 0;
+    }
 
 }
 
