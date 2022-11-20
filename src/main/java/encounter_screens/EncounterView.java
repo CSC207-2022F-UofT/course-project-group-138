105110f3fcd360fcbdc09ca229086d0148ab9480
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-11-19 22:36:20 -0500

    Implemented Player and Enemy
    Added interface Inv, which ensures Inventory has all the methods Player and Enemy depend on
    
    Merged Equipment and Purchasable into one interface
    
    Moved test folder

--- src/main/java/encounter_screens/EncounterView.java
+++ src/main/java/encounter_screens/EncounterView.java
@@ -2,11 +2,11 @@ package encounter_screens;
 
 abstract class EncounterView {
     /**
-     * Possible GUI for interaction with Enemy and merchant_interactions.Merchant
+     * Possible GUI for interaction with character.Enemy and merchant_interactions.Merchant
      */
 
     /**
-     * Method for interacting with either merchant_interactions.Merchant or Enemy.
+     * Method for interacting with either merchant_interactions.Merchant or character.Enemy.
      * Triggers corresponding action.
      */
     public abstract void action();
