4ebf1c90348245567c7b78e7f2a3bb9d15f2e882
bigman4real <yi.kk922@gmail.com>
2022-11-21 17:46:01 -0500

    test view

--- src/main/java/encounter_screens/TestView.java
+++ src/main/java/encounter_screens/TestView.java
@@ -1,2 +1,30 @@
-package encounter_screens;public class TestView {
+package encounter_screens;
+
+
+public class TestView {
+    public static void main(String[] args) {
+
+        EnemyEncounterView eev = new EnemyEncounterView();
+        MerchantEncounterView mev = new MerchantEncounterView();
+
+
+
+/*        ImageIcon image = new ImageIcon();
+
+        JLabel label = new JLabel();
+        label.setText("ccccccccc");
+        // lable.setIcon(image);
+        myFrame.add(label);
+
+ */
+
+
+
+
+
+
+
+
+
+    }
 }
