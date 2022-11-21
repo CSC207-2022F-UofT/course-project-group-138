a940dd61da65bc792de4b695e5eb2e98fe1ab9dd
bigman4real <yi.kk922@gmail.com>
2022-11-21 11:11:58 -0500

    GamePanel Draft Version

---
+++ src/main/java/GamePanel/GamePanel.java
@@ -0,0 +1,36 @@
+package GamePanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    final int originalSize = 16;
+    final int scale = 4;
+    final int tileSize = originalSize * scale;
+    final int row = 12;
+    final int col = 16;
+    final int screenWidth = tileSize * col;
+    final int screenHeight = tileSize * row;
+
+    Thread gameThread;
+    public GamePanel () {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(new Color(0x12345));
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        //while loop for game loop
+
+    }
+
+
+}
