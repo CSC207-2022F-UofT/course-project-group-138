99d575c4ad3ae076cb84c008cc04043082ed142c
Ocean-Ch <oceanchenn@gmail.com>
2022-11-21 11:01:43 -0500

    tests

---
+++ src/main/tests/PlayerMoverTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import presenters.PlayerMover;
+import character.Player;
+
+public class PlayerMoverTests{
+    public void testPlayerPosition{
+        Player player = new Player()
+    }
+}
