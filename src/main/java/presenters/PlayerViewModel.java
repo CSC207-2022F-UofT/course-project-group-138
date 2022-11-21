1495a2b46d2e09ac8d2a25fa08d712ee5383d58f
Ocean-Ch <oceanchenn@gmail.com>
2022-11-21 12:13:41 -0500

    PlayerViewModel tests, PlayerMover tests.
    Created setPlayerImage method in PlayerViewModel.

--- src/main/java/presenters/PlayerViewModel.java
+++ src/main/java/presenters/PlayerViewModel.java
@@ -1,11 +1,9 @@
 package presenters;
+
 import character.Player;
 import settings.Settings;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 /*
     Purpose of this class is to segregate back-end business logic and front-end GUI. Refer to the MVVM architectural
     pattern.
@@ -16,9 +14,9 @@ import java.io.IOException;
 public class PlayerViewModel extends Rectangle {
     private final Player player;
     // Gets the user drawn player image from "res"
-    private final Image playerImage = ImageIO.read(new File("res/character.png"));
+    private Image playerImage;
 
-    public PlayerViewModel(Player player) throws IOException {
+    public PlayerViewModel(Player player) {
         super(player.getx() * Settings.getPlayerSize(), player.gety() * Settings.getPlayerSize(),
                 Settings.getPlayerSize(), Settings.getPlayerSize());
         this.player = player;
@@ -31,7 +29,9 @@ public class PlayerViewModel extends Rectangle {
         super.x = player.getx();
         super.y = player.gety();
     }
-
+    public void setPlayerImage(Image image){
+        playerImage = image;
+    }
     /**
      * This will draw the user's drawn image onto graphics
      * @param graphics - Graphics object in which the player will be drawn.
