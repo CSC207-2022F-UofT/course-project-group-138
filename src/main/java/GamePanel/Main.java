5ba778362983a453e3980afb265ba29e96b2caf2
bigman4real <yi.kk922@gmail.com>
2022-11-21 17:46:55 -0500

    note of game panel

--- src/main/java/GamePanel/Main.java
+++ src/main/java/GamePanel/Main.java
@@ -4,24 +4,21 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame window = new JFrame();
         window.setVisible(true);
-        window.setTitle("wattttt");
+        window.setTitle("Amazing Dungeon Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of app
         window.setResizable(false); //can't resize
         window.setLocationRelativeTo(null); //centered
 
-
-
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
-
 
         window.pack(); //fit the size
 
 
-
-        gamePanel.startGameThread();
+        gamePanel.startGameThread(); //will initiate the game loop through the gamepanel class
 
 
 
