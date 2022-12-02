527398a553e454d9bf08bb3835304939a163005e
bigman4real <yi.kk922@gmail.com>
2022-12-02 18:56:42 -0500

    Plausible GUI for abstract EncounterView. Updated version of merchant and enemy view with images filed in res folder.
    (possible improvement, make interface to shorten the code.

--- src/main/java/UI/encounter_screens/EnemyEncounterView.java
+++ src/main/java/UI/encounter_screens/EnemyEncounterView.java
@@ -1,43 +1,64 @@
 package UI.encounter_screens;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class EnemyEncounterView extends JFrame implements ActionListener {
+public class EnemyEncounterView extends EncounterView {
+
+    /**
+     * The window that will be poped up, when the player hits enemies.
+     * Provide more specific implementation of the EncounterView.
+     */
     private JButton attack;
 
-    EnemyEncounterView() {
+    EnemyEncounterView() throws IOException {
 
 
-        this.setVisible(true);
-        this.setSize(630, 420);
-        this.setTitle("EnemyEncountered");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of app
-        this.setResizable(false); // can't resize
-        this.setLocationRelativeTo(null); //centered
-        // ImageIcon image = new ImageIcon("logo.png");
-        // frame.setIconImage(image.getImage());
-        this.setLayout(null);
-        this.getContentPane().setBackground(new Color(0x12345)); //rgb value
+        this.setTitle("EnemyEncountered"); // set the title of the frame
 
+        BufferedImage img = ImageIO.read(new File("src/main/res/regular_enemy.png")); // read the image.
+        Image dimg = img.getScaledInstance(200, 240, Image.SCALE_SMOOTH); // rescale the image displayed
+        ImageIcon imageIcon = new ImageIcon(dimg); // create an instance of the image
 
-        attack = new JButton("attack");
+        label = new JLabel(); //JLabel Creation
+        label.setBounds(215, 55, 200, 240);
+        label.setIcon(imageIcon); //Sets the image to be displayed as an icon
+
+        c = this.getContentPane(); //Gets the content layer
+        c.setBackground(new Color(0x12345)); //rgb value, set the background color
+        c.add(label); //Adds objects to the container
+
+        attack = new JButton("Attack");
         attack.setBounds(255, 350, 120, 30);
-
         attack.addActionListener(this);
+        c.add(attack); // Add a attack button on the container.
 
-        add(attack);
+        leave = new JButton("Leave");
+        leave.setBounds(480, 350, 120, 30);
+        leave.addActionListener(this);
+        c.add(leave); // Add a leave button on the container.
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == attack){
-            System.out.println("Ha");
+            System.out.println("Ouch!!!");
 
             //should incorporate the features in player and enemy.
             JOptionPane.showMessageDialog(null, "oh no you died :(");
+        }
+
+        if (e.getSource() == leave){
+            System.out.println("Bye");
+            // also links back to the main play state
+            // TODO Incorporate more functionality
         }
     }
 }
--- src/main/java/UI/encounter_screens/MerchantEncounterView.java
+++ src/main/java/UI/encounter_screens/MerchantEncounterView.java
@@ -3,43 +3,58 @@ package UI.encounter_screens;
 import entities.character.Player;
 import useCases.merchantUseCases.PurchaseController;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class MerchantEncounterView extends JFrame implements ActionListener {
+public class MerchantEncounterView extends EncounterView{
+
+    /**
+     * The Window will be poped up when the player hits the merchant.
+     * The according image will be shown, as well as the according features, like purchase, and leave.
+     */
     private JButton purchase;
     private Player player;
     private PurchaseController purchaseController;
 
-    MerchantEncounterView(Player player) {
+    MerchantEncounterView(Player player) throws IOException {
 
-        this.setVisible(true);
-        this.setSize(630, 420);
-        this.setTitle("MerchantEncountered");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of app
-        this.setResizable(false); // can't resize
-        this.setLocationRelativeTo(null); //centered
-        // ImageIcon image = new ImageIcon("logo.png");
-        // frame.setIconImage(image.getImage());
-        this.setLayout(null);
-        this.getContentPane().setBackground(new Color(0x12345)); //rgb value
+        this.setTitle("MerchantEncountered"); // set the title of the frame
+
+        BufferedImage img = ImageIO.read(new File("src/main/res/sample_merchant.png")); // read the image.
+        Image dimg = img.getScaledInstance(200, 240, Image.SCALE_SMOOTH); // rescale the image displayed
+        ImageIcon imageIcon = new ImageIcon(dimg); // create an instance of the image
+
+        label = new JLabel(); //JLabel Creation
+        label.setBounds(215, 55, 200, 240);
+        label.setIcon(imageIcon); //Sets the image to be displayed as an icon
+
+        c = this.getContentPane(); //Gets the content layer
+        c.setBackground(new Color(0xECE592)); //rgb value, set the background color
+        c.add(label); //Adds objects to the container
+
+        purchase = new JButton("Purchase");
+        purchase.setBounds(255, 350, 120, 30);
+        purchase.addActionListener(this);
+        c.add(purchase); // Add a purchase button on the container.
+
+        leave = new JButton("Leave");
+        leave.setBounds(480, 350, 120, 30);
+        leave.addActionListener(this);
+        c.add(leave); // Add a leave button on the container.
+
+        // TODO: will add more buttons that characterize the features in the merchant class
 
         this.player = player;
         purchaseController = new PurchaseController(player, "Weapon");
+
         //TODO: Change based on player input
 
-        purchase = new JButton("purchase");
-        purchase.setBounds(255, 350, 120, 30);
-
-        purchase.addActionListener(this);
-
-        add(purchase);
-
-
-
-        // will add more buttons that characterize the features in the merchant class
     }
 
     @Override
@@ -48,6 +63,12 @@ public class MerchantEncounterView extends JFrame implements ActionListener {
             purchaseController.purchaseCheck();
 
             //should incorporate more features
+        }
+
+        if (e.getSource() == leave){
+            System.out.println("Bye");
+            // also links back to the main play state
+            // TODO Incorporate more functionality
         }
     }
 }
