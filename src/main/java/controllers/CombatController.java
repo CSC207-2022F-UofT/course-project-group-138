3c737cc646d3dce042921c1525c42fb6328b834e
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-12-06 18:01:09 -0500

    create CombatController

--- src/main/java/controllers/CombatController.java
+++ src/main/java/controllers/CombatController.java
@@ -1,11 +1,28 @@
 package controllers;
 
+import useCases.Attack;
+import entities.character.Character;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+
 public class CombatController {
     /**
      * Controller for facillating data between the user interface and CombatLoop
      */
 
-    public CombatController(String userinput) {
+    /**
+     * Passes two characters and user input into Attack.taketurnhp()
+     * @param userInput
+     */
+    public void CombatController(Character char1, Character char2, String userInput) {
+        // TODO: Replace String with GUI.Button presses
+        if (userInput.equals("Attack")) {
+            List<Integer> updatedHP = Attack.taketurnhp(char1, char2);
+        }
+    }
+
+    public CombatController(ActionEvent e) {
 
     }
 }
