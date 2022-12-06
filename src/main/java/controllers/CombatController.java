a2cefbb8abf355967b27f730bb4c343a752661d6
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-12-06 17:40:11 -0500

    saving progress to update branch

---
+++ src/main/java/controllers/CombatController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+public class CombatController {
+    /**
+     * Controller for facillating data between the user interface and CombatLoop
+     */
+
+    public CombatController(String userinput) {
+
+    }
+}
