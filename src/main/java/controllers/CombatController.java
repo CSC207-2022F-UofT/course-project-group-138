9099994d0b3d2dea4b2bb4fb2dd318b19768294e
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-12-07 03:49:03 -0500

    minor modifications to CombatController

--- src/main/java/controllers/CombatController.java
+++ src/main/java/controllers/CombatController.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import entities.character.Enemy;
+import entities.character.Player;
 import useCases.Attack;
 import entities.character.Character;
+import useCases.CombatRound;
 
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CombatController {
@@ -12,29 +14,37 @@ public class CombatController {
      * Controller for facillating data between the user interface and CombatLoop
      */
 
+    private final Player player;
+    private final Enemy enemy;
+
+
+    /**
+     *  === Constructor ===
+     * @param player - character 1
+     * @param enemy - character 2
+     * @param e     - input
+     */
+    public CombatController(Player player, Enemy enemy, ActionEvent e) {
+        this.player = player;
+        this.enemy = enemy;
+    }
+
     /**
      * Passes two characters and user input into Attack.taketurnhp()
-     * @param char1     - Character that attacks first (player)
-     * @param char2     - Character that attacks second (enemy)
+     * This method is open for extension, adding a switch statement and a variety of cases allows the controller
+     * to respond to a variety of string inputs, such as defending, retreating, or using an item.
+     *
      * @param userInput - user input
      * @return A List of ints, first entry is char1's updated hp, second entry is char2's updated hp
      */
-    public List<Integer> combatTurn(Character char1, Character char2, String userInput) {
-        // TODO: Replace String with GUI.Button presses
+    public List<Integer> combatTurn(String userInput) {
+        // Does "Attack" need to be replaced with GUI.Button presses?
         List<Integer> updatedHP = null;
         if (userInput.equals("Attack")) {
-            updatedHP = Attack.taketurn(char1, char2);
+            updatedHP = CombatRound.combatRound(this.player, this.enemy);
         }
         return updatedHP;
     }
 
-    /**
-     * Constructor
-     * @param char1 - character 1
-     * @param char2 - character 2
-     * @param e     - input
-     */
-    public CombatController(Character char1, Character char2, ActionEvent e) {
-        combatTurn(char1, char2, "Attack");
-    }
+
 }
