0f51f109263b3f5c70dbb0b7aaa6105839e17eb0
bigman4real <yi.kk922@gmail.com>
2022-12-06 21:28:57 -0500

    Extended from encounterview class. More detailed when encountering an enemy.

--- src/main/java/UI/encounter_screens/EnemyEncounterView.java
+++ src/main/java/UI/encounter_screens/EnemyEncounterView.java
@@ -1,43 +1,72 @@
 package UI.encounter_screens;
 
+import controllers.StateManager;
+import controllers.gameStates.CrawlingState;
+import gateways.ImageGateway;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
-public class EnemyEncounterView extends JFrame implements ActionListener {
+public class EnemyEncounterView extends EncounterView {
+
+    /**
+     * The window that will be poped up, when the player hits enemies.
+     * Provide more specific implementation of the EncounterView.
+     */
     private JButton attack;
+
 
     EnemyEncounterView() {
 
 
-        this.setVisible(true);
-        this.setSize(630, 420);
-        this.setTitle("EnemyEncountered");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of app
-        this.setResizable(false); // can't resize
-        this.setLocationRelativeTo(null); //centered
-        // ImageIcon image = new ImageIcon("logo.png");
-        // frame.setIconImage(image.getImage());
-        this.setLayout(null);
-        this.getContentPane().setBackground(new Color(0x12345)); //rgb value
+        this.setTitle("EnemyEncountered"); // set the title of the frame
 
+        BufferedImage img = ImageGateway.getEnemyImg(); // read the image.
+        Image dimg = img.getScaledInstance(200, 240, Image.SCALE_SMOOTH); // rescale the image displayed
+        ImageIcon imageIcon = new ImageIcon(dimg); // create an instance of the image
 
-        attack = new JButton("attack");
-        attack.setBounds(255, 350, 120, 30);
+        label = new JLabel(); //JLabel Creation
+        label.setBounds(215, 55, 200, 240);
+        label.setIcon(imageIcon); //Sets the image to be displayed as an icon
 
+        c = this.getContentPane(); //Gets the content layer
+        c.setBackground(new Color(0x123456)); //rgb value, set the background color
+        c.add(label); //Adds objects to the container
+
+        attack = new JButton("Attack");
+        attack.setBounds(30, 290, 120, 90);
         attack.addActionListener(this);
+        attack.setFont(new Font("Comic Sans", Font.BOLD, 20));
+        c.add(attack); // Add a attack button on the container.
 
-        add(attack);
+        exit = new JButton("Exit");
+        exit.setBounds(480, 290, 120, 90);
+        exit.addActionListener(this);
+        exit.setFont(new Font("Comic Sans", Font.BOLD, 20));
+        c.add(exit); // Add a leave button on the container.
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == attack){
-            System.out.println("Ha");
+            // System.out.println("Ouch!!!");
 
             //should incorporate the features in player and enemy.
             JOptionPane.showMessageDialog(null, "oh no you died :(");
+            // TODO Incorporate more functionality. Refer to MerchantEncounterView.java.
+
+            //attack.setEnabled(false);
+        }
+
+        if (e.getSource() == exit){
+            System.exit(0);
         }
     }
+
+    // TODO: Display enemy's and player's current HP after one round (after the user clicks the button) on GUI.
+
 }
