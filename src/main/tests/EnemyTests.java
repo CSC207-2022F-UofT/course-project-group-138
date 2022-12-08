038b1d4e53342a4fee4fa07eebe49ea138da7ae1
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-12-08 01:22:30 -0500

    added basic tests for Entity.java's methods, hopefully bumps up code coverage %

--- src/main/tests/EnemyTests.java
+++ src/main/tests/EnemyTests.java
@@ -1,16 +1,24 @@
+import entities.character.Enemy;
+import entities.inventory.Armor;
+import entities.inventory.Inventory;
+import entities.inventory.Weapon;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class EnemyTests {
 
-    @Test(timeout = 500)
-    public void testSample() {
-        int a = 0;
-        int b = 0;
+    private static Enemy enemy;
 
-        // assertion
-        assertEquals(a, b);
+    public void initializeEnemy() {
+        enemy = new Enemy(new Inventory(20, new Weapon(10), new Armor(10)), 20, 10, 10);
+    }
+
+    /**
+     * Tests whether x,y coords remain constant while directions set to false, and that default is set to false.
+     */
+    @Test
+    public void testMovePlayerFalse(){
+        initializeEnemy();
+        assert enemy.getX() == 0 : enemy.getY() == 0;
     }
 
 }
