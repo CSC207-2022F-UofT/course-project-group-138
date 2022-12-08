700feed1169e8624b2f4609f1bc2d5f8b1ee5ee5
hanyu-xiao <hanyu.xiao@mail.utoronto.ca>
2022-12-08 01:28:34 -0500

    added basic tests for Player.java's methods, hopefully bumps up code coverage %

--- src/main/tests/PlayerTests.java
+++ src/main/tests/PlayerTests.java
@@ -1,16 +1,37 @@
+import entities.character.Enemy;
+import entities.character.Player;
+import entities.inventory.Armor;
+import entities.inventory.Inventory;
+import entities.inventory.Weapon;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class PlayerTests {
 
-    @Test(timeout = 500)
-    public void testSample() {
-        int a = 0;
-        int b = 0;
+    private static Player player;
 
-        // assertion
-        assertEquals(a, b);
+    public void initializePlayer() {
+        player = new Player(new Inventory(20, new Weapon(10), new Armor(10)), 20, 10, 10);
+    }
+
+    /**
+     * Tests overloaded constructor without a <kills> parameter
+     */
+    @Test
+    public void testConstructor(){
+        initializePlayer();
+        assert player.getKills() == 0;
+    }
+
+    /**
+     * Tests getKills() and increaseKills()
+     */
+    @Test
+    public void testKills(){
+        player.increaseKills();
+        player.increaseKills();
+        player.increaseKills();
+        assert player.getKills() == 3;
     }
 
 }
